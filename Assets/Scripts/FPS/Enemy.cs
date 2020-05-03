46b391d277e968ae5ea4fe8d68e75cf067b02f9b
Iker <ikernoya4@gmail.com>
2020-05-03 15:47:17 -0300

    Created shooting mechanics

---
+++ Assets/Scripts/FPS/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 100.0f;
+    float timerLimit = 3;
+    bool dead = false;
+    float timerDestroy;
+    private void Start()
+    {
+        timerDestroy = 0;
+    }
+
+    private void Update()
+    {
+        if (dead == true)
+        {
+            timerDestroy += Time.deltaTime;
+            Debug.Log(timerDestroy);
+            if (timerDestroy >= timerLimit)
+            {
+                Die();
+            }
+        }
+    }
+    public void takeDamage(float ammount)
+    {
+        health -= ammount;
+        if (health <= 0)
+        {
+            dead = true;
+        }
+    }
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
+
--- Assets/Scripts/FPS/PlayerInput.cs
+++ Assets/Scripts/FPS/PlayerInput.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    RaycastHit hit;
+    public LayerMask rayLayer;
 
-    void Start()
-    {
-        
-    }
+    public float range;
+    public float damage;
 
 
     void Update()
     {
-        
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+        }
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, rayLayer))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot(transform.position, hit.transform.position);
+        }
+    }
+
+    void shoot(Vector3 from, Vector3 to)
+    {
+        Vector3 direction = to-from;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+
+        }
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, rayLayer))
+        {
+           Enemy enemy =  hit.transform.GetComponent<Enemy>();
+           Rigidbody rigid = hit.transform.GetComponent<Rigidbody>();
+            if (enemy!=null)
+            {
+                enemy.takeDamage(damage);
+                if (enemy.health <= 0)
+                {
+                    rigid.AddForce(direction * 200);
+                }
+            }
+        }
     }
 }
