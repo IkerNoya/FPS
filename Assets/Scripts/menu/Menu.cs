82d70ca3c1d92cb69f9ebe557e6c4f72ced368d7
Iker <ikernoya4@gmail.com>
2020-05-03 18:14:41 -0300

    Completed menu scene

---
+++ Assets/Scripts/menu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnClickGame()
+    {
+        SceneManager.LoadScene("fps-game");
+    }
+    public void OnClickCredits()
+    {
+        SceneManager.LoadScene("fps-credits");
+    }
+}
